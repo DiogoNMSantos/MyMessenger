5404db2c5def42ed6de2241e353bf5f29104bda5
Diogo <diogo@Athanor.local>
2021-10-17 18:31:04 +0200

    Added second activity

--- app/src/main/java/com/diogonmsantos/mymessenger/RecieveMessageActivity.kt
+++ app/src/main/java/com/diogonmsantos/mymessenger/RecieveMessageActivity.kt
@@ -3,11 +3,9 @@ package com.diogonmsantos.mymessenger
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RecieveMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_recieve_message)
     }
-
-    fun onSendMessage(view: android.view.View) {}
 }
