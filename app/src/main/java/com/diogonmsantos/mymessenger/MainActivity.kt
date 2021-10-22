96b34620424c70e818c558317a0caac5d4ed51ae
Diogo <diogo@Athanor.local>
2021-10-22 20:10:35 +0200

    impliment intent

--- app/src/main/java/com/diogonmsantos/mymessenger/MainActivity.kt
+++ app/src/main/java/com/diogonmsantos/mymessenger/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.diogonmsantos.mymessenger
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun onSendMessage(view: android.view.View) {}
+    fun onSendMessage(view: android.view.View) {
+//        val intent = Intent(this, ReceiveMessageActivity::class.java)
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+
+        val messageView = findViewById<EditText>(R.id.message)
+        val messageText = messageView.text.toString()
+
+        intent.putExtra(Intent.EXTRA_TEXT, messageText)
+        val chooserTitle = getString(R.string.chooser)
+        val chosenIntent = Intent.createChooser(intent, chooserTitle)
+//        intent.putExtra("message", messageText)
+        startActivity(chosenIntent)
+    }
 }
--- app/src/main/java/com/diogonmsantos/mymessenger/RecieveMessageActivity.kt
+++ app/src/main/java/com/diogonmsantos/mymessenger/RecieveMessageActivity.kt
@@ -1,11 +1,15 @@
 package com.diogonmsantos.mymessenger
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 
-class RecieveMessageActivity : AppCompatActivity() {
+class ReceiveMessageActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recieve_message)
+        val message = intent.getStringExtra("message")
+        val messageView = findViewById<TextView>(R.id.message)
+        messageView.text = message
     }
 }
